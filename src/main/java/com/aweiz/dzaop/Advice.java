f64b121836ba759436cc044c4508aa5b60cde0ca
AntraJava <dawei.zhuang@antra.net>
2021-03-12 14:12:39 -0500

    Update Advice.java

--- src/main/java/com/aweiz/dzaop/Advice.java
+++ src/main/java/com/aweiz/dzaop/Advice.java
@@ -3,7 +3,7 @@ package com.aweiz.dzaop;
 /**
  * Not functional. All are AROUND now.
  * Created by daweizhuang on 5/24/16.
- * This is a test line - update from github
+ * This is a test line - update from github - udpate 2nd
  */
 public enum Advice {
     BEFORE,AFTER,AROUND
